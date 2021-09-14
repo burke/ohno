1722e60ea3d531c215b523560c0706b00e8d2dc3
Burke Libbey <burke@libbey.me>
2021-09-14 15:06:43 -0400

    .

--- test.c
+++ test.c
@@ -42,56 +42,12 @@ int main(int argc, char **argv){
 
 	pid_t ns_pid = getpid();
 
-	// Hook the target.
 	target = ptrace_do_init(pid);
 
 	int pidfd = ptrace_do_syscall(target, __NR_pidfd_open, ns_pid, 0, 0, 0, 0, 0);
 	int ret = ptrace_do_syscall(target, __NR_setns, pidfd, CLONE_NEWNET, 0, 0, 0, 0);
 	int ret2 = ptrace_do_syscall(target, __NR_close, pidfd, 0, 0, 0, 0, 0);
 
-	/*
-	// Demonstrating memory allocation in the target process.
-	string1 = (char *) ptrace_do_malloc(target, BUFF_LEN);
-	memset(string1, 0, BUFF_LEN);
-	// Treat the local string as you would normally.
-	snprintf(string1, BUFF_LEN, "foo\n");
-	// Then, when its all set, push it into the remote processes memory. It will know the right spot automatically.
-	tmp_addr = ptrace_do_push_mem(target, string1);
-	// Now that it's in the remote memory we can remotely call the write() syscall, and point it at the remote address.
-	ptrace_do_syscall(target, __NR_write, 1, (unsigned long) tmp_addr, strnlen(string1, BUFF_LEN), 0, 0, 0);
-
-	// Lets do it a couple more times with different buffer sizes.
-	string2 = (char *) ptrace_do_malloc(target, BUFF_LEN - 3);
-	memset(string2, 0, BUFF_LEN - 3);
-	snprintf(string2, BUFF_LEN - 3, "bar\n");
-	tmp_addr = ptrace_do_push_mem(target, string2);
-	ptrace_do_syscall(target, __NR_write, 1, (unsigned long) tmp_addr, strnlen(string2, BUFF_LEN - 3), 0, 0, 0);
-
-	// One more time...
-	string3 = (char *) ptrace_do_malloc(target, BUFF_LEN - 5);
-	memset(string3, 0, BUFF_LEN - 5);
-	snprintf(string3, BUFF_LEN - 5, "baz\n");
-	tmp_addr = ptrace_do_push_mem(target, string3);
-	ptrace_do_syscall(target, __NR_write, 1, (unsigned long) tmp_addr, strnlen(string3, BUFF_LEN - 5), 0, 0, 0);
-
-	// Throw in a sleep(60); in here if you want to pause and go examine the targets /proc/PID/maps file.
-
-	// Let's clear the memory in the local buffers...
-	memset(string2, 0, BUFF_LEN - 3);
-	memset(string3, 0, BUFF_LEN - 5);
-
-	// and now demonstrate that we can pull the data from the remote memory locations.
-	tmp_addr = ptrace_do_pull_mem(target, string3);
-	// Here you'll see the address in the remote memory being known and printed locally.
-	printf("DEBUG: tmp_addr: %p\n", tmp_addr);
-	ptrace_do_syscall(target, __NR_write, 1, (unsigned long) tmp_addr, strnlen(string3, BUFF_LEN - 5), 0, 0, 0);
-
-	tmp_addr = ptrace_do_pull_mem(target, string2);
-	printf("DEBUG: tmp_addr: %p\n", tmp_addr);
-	ptrace_do_syscall(target, __NR_write, 1, (unsigned long) tmp_addr, strnlen(string2, BUFF_LEN - 3), 0, 0, 0);
-	*/
-
-	// Unhook and clean up.
 	ptrace_do_cleanup(target);
 
 	return(0);
